a4d6bfcfee1e1ab3cbb3c2eacbf4ca7fe4dd65ac
programmerXS <cristoferperezmejia80@gmail.com>
2022-07-05 18:23:50 -0400

    Add normalize.css

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./styles/normalize.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
