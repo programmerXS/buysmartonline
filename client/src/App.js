8d949bd8501963a69087b87aa92e54b919a8b711
programmerXS <cristoferperezmejia80@gmail.com>
2022-07-05 18:38:12 -0400

    Add react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+
 const App = () => {
-  return <div className="App">hola</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
