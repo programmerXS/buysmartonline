c41ac677978af054f549e3e6359daa85b42ddc75
programmerXS <cristoferperezmejia80@gmail.com>
2022-07-24 18:09:37 -0400

    Add images and headers link

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,57 @@
+import logo from "../styles/images/logo.png";
+import { Link } from "react-router-dom";
+import barsChart from "../styles/images/barChart.svg";
+import heart from "../styles/images/heart.svg";
+import cartIcon from "../styles/images/cart.svg";
+import menuIcon from "../styles/images/menu.svg";
+import HomeHeader from "../styles/HomeStyles";
+
 const Home = () => {
-  return <h1>Hola</h1>;
+  return <Header />;
+};
+
+const Header = () => {
+  return (
+    <HomeHeader>
+      <div>
+        <img src={logo} alt="logo" />
+      </div>
+      <nav>
+        <div></div>
+        <>
+          <SearchBar />
+        </>
+      </nav>
+      <nav>
+        <Link to="/compare">
+          <img className="icon" src={barsChart} alt="Compare" />
+        </Link>
+        <Link to="/wishlist">
+          <img className="icon" src={heart} alt="Wish list" />
+        </Link>
+        <Link to="/cart">
+          <img className="icon" src={cartIcon} alt="Cart" />
+        </Link>
+      </nav>
+    </HomeHeader>
+  );
+};
+
+const SearchBar = () => {
+  return (
+    <div>
+      <input placeholder="I'm shopping for..." />
+      <button>Search</button>
+    </div>
+  );
+};
+
+const Menu = () => {
+  return (
+    <div>
+      <img srt={menuIcon} alt="menu" />
+    </div>
+  );
 };
 
 export default Home;
---
+++ client/src/styles/HomeStyles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const HomeHeader = styled.header`
+  display: grid;
+  grid-template-columns: 350px 25rem 25rem;
+  background-color: orange;
+
+  .icon {
+    width: 9%;
+  }
+`;
+
+export const HomeSearchBar = styled.div``;
+export const HomeMenu = styled.div``;
+
+export default HomeHeader;
